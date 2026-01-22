8a9f85b0eceb994e3cdb8bbcf7d378c19ec5af04
Grigory  Dubikov <grin-dubikov@yandex.ru>
2026-01-22 18:10:07 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 250; //цена билета в рублях
+        int mile = 20; //стоимость одной мили в рублях
+        int bonus = ticket / mile;
+
+        System.out.println(bonus);
+    }
+}
